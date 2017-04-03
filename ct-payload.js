ceb9acc8e7078f1388bda8172c594977c64cb7bb
pandabrand <pandabrand@gmail.com>
2017-04-03 11:28:55 -0500

    going back to jsut using css for responsiveness, cloudinary not working correectly

--- ct-payload.js
+++ ct-payload.js
@@ -58,11 +58,8 @@
           //city image div
           let city_image_div = document.createElement('div');
           city_image_div.className = 'flex-item cc-body cc-image';
-          city_image_url = $.cloudinary.url(city.guidePreview, {cloud_name: 'hjoyay5gd', width:'auto'});
-          city_image = document.createElement('img');
-          city_image.setAttribute('data-src', city_image_url);
-          city_image.className = 'cld-responsive';
-          city_image_div.appendChild(city_image);
+          let city_image = $.cloudinary.image(city.guidePreview, {cloud_name: 'hjoyay5gd', width:'auto', dpr:'auto', responsive_placeholder: 'blank'});
+          $(city_image_div).append(city_image);
           cc_info.appendChild(city_image_div);
 
           //create location scoller
@@ -95,7 +92,7 @@
 
               let location_image_div = document.createElement('div');
               location_image_div.className = 'cc-location-image-div flex-item-image';
-              location_image = $.cloudinary.image(location.photo, {cloud_name: 'hjoyay5gd', width:'auto', dpr:'auto', responsive_placeholder: "blank"});
+              location_image = $.cloudinary.image(location.photo, {cloud_name: 'hjoyay5gd', width:'auto', dpr:'auto', responsive_placeholder: 'blank'});
               $(location_image_div).append(location_image);
               location_row.appendChild(location_image_div);
 
