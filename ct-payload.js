3c79dd1bc0dafd544501fcd4f7ae3fef22d0f039
pandabrand <pandabrand@gmail.com>
2017-04-03 10:31:18 -0500

    added classname declartion

--- ct-payload.js
+++ ct-payload.js
@@ -57,7 +57,7 @@
 
           //city image div
           let city_image_div = document.createElement('div');
-          city_image_div = 'flex-item cc-body cc-image';
+          city_image_div.className = 'flex-item cc-body cc-image';
           city_image = $.cloudinary.image(city.guidePreview, {cloud_name: 'hjoyay5gd', height:300, dpr:'2.0'});
           $(city_image_div).appendChild(city_image);
           cc_info.appendChild(city_image_div);
